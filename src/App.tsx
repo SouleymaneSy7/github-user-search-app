09651c050e7e206c004fc37c3e57adabb9a049ce
Souleymane Sy <souleymanesyservices@gmail.com>
2024-10-16 13:17:55 +0000

    feat: add the Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Forms from "./components/Forms";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <React.Fragment>
       <Header />
       <Forms />
+      <Main />
     </React.Fragment>
   );
 };
