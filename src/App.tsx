8defc6ce51e6d6c9991b030b3901b5ecf706ef9e
Souleymane Sy <souleymanesyservices@gmail.com>
2024-10-17 00:24:29 +0000

    feat: updated the HTML structure of the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from "react";
 import Header from "./components/Header";
-import Forms from "./components/Forms";
 import Main from "./components/Main";
+import { UserContextProvider } from "./contexts/UserContext";
+import { ThemeContextProvider } from "./contexts/ThemeContext";
+import Forms from "./components/Forms";
 
 const App = () => {
+  const selectedTheme = localStorage.getItem("theme");
+
+  if (selectedTheme) {
+    document.querySelector("body")?.setAttribute("data-theme", selectedTheme);
+  }
+
   return (
-    <React.Fragment>
-      <Header />
-      <Forms />
-      <Main />
-    </React.Fragment>
+    <UserContextProvider>
+      <ThemeContextProvider>
+        <Header />
+        <Forms />
+        <Main />
+      </ThemeContextProvider>
+    </UserContextProvider>
   );
 };
 
