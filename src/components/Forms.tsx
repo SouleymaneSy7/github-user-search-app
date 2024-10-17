0953f452801c0d657383a28b7b7afe63a4f42e31
Souleymane Sy <souleymanesyservices@gmail.com>
2024-10-17 00:23:14 +0000

    feat: update the Forms component

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { FC } from "react";
 
 import Inputs from "./Inputs";
 import Buttons from "./Buttons";
 import VisuallyHidden from "./VisuallyHidden";
 import { SearchIcon } from "../icons/Icons.component";
+import { useUserContext } from "../contexts/UserContext";
 
-const Forms = () => {
-  const id = React.useId();
-  const inputID = `input-${id}`;
+const Forms: FC = () => {
+  const id: string = React.useId();
+  const inputID: string = `input-${id}`;
+
+  const { searchTerm, handleSearch, handleGetUser } = useUserContext();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    handleGetUser();
+  };
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <label htmlFor={inputID} className="form__label">
         <SearchIcon />
         <VisuallyHidden>Search Icons</VisuallyHidden>
       </label>
-      <Inputs id={inputID} type="search" className="form--input" />
+      <Inputs
+        id={inputID}
+        type="search"
+        className="form--input"
+        value={searchTerm}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+          handleSearch(event);
+        }}
+      />
       <Buttons type="submit" className="form--btn">
         Search
       </Buttons>
