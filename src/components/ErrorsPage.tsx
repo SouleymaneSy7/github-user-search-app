50981523d9bc9ec68f244d62164376ce2c400ad1
Souleymane Sy <souleymanesyservices@gmail.com>
2024-10-18 11:24:17 +0000

    feat: updated the html structure

--- src/components/ErrorsPage.tsx
+++ src/components/ErrorsPage.tsx
@@ -2,11 +2,11 @@ import { ErrorsIcon } from "../icons/Icons.component";
 
 const ErrorsPage = () => {
   return (
-    <div className="errors">
+    <div className="errors | container">
       <div className="errors-icons">
         <ErrorsIcon />
       </div>
-      <p>404 Error Not Found!</p>
+      <p>Username Not Found!</p>
     </div>
   );
 };
