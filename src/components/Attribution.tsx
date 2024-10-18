7f00fec48bf76905100bae99401e9ca386f43382
Souleymane Sy <souleymanesyservices@gmail.com>
2024-10-18 11:28:58 +0000

    feat: add the owner attribution

---
+++ src/components/Attribution.tsx
@@ -0,0 +1,26 @@
+const Attribution = () => {
+  return (
+    <footer className="container">
+      <section className="attribution">
+        <p>
+          Inspired from the challenge on{" "}
+          <a
+            href="https://www.frontendmentor.io/challenges/github-user-search-app-Q09YOgaH6"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+        </p>
+
+        <p>
+          Code 😎️ by{" "}
+          <a href="https://github.com/SouleymaneSy7" target="_blank">
+            Souleymane Sy
+          </a>
+        </p>
+      </section>
+    </footer>
+  );
+};
+
+export default Attribution;
